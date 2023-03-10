2e34bc8537e15dd19c6659bbf6f22f2a78d6ea74
douguya <imamurasyunnsuke.patagonia@gmail.com>
2023-03-10 18:18:02 +0900

    no message

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -6,19 +6,33 @@ public class Bullet : MonoBehaviour
 {
 
 
-    public int Bullet_Speed;
+    public int Bullet_Speed;//’e‘¬
+
+    public int Ballet_Range;//ŽË’ö‹——£
+
+    public Vector3 StartPosi;//”­ŽËŽž‚ÌˆÊ’u
     // Start is called before the first frame update
     void Start()
     {
-      
+        StartPosi = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         this.transform.Translate(Vector3.right * Time.deltaTime * Bullet_Speed);
+
+        if (Vector3.Distance(transform.position, StartPosi)> Ballet_Range)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
 
 
 }
